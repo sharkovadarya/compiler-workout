6680902c9937b7b030d1bcff202dc7f79c52fc22
sharkovadarya <sharkovadarya@gmail.com>
2018-09-18 11:07:07 +0300

    hw3 initial commit

--- src/SM.ml
+++ src/SM.ml
@@ -18,13 +18,26 @@ type prg = insn list
  *)
 type config = int list * Stmt.config
 
+(* Utility functions used for 'eval' function *)
+
+let eval_bin_op op (y :: x :: st, c) = ((Language.Expr.eval_bin_op op x y) :: st, c)
+
 (* Stack machine interpreter
 
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let rec eval (st, (s, i, o)) p = match p with 
+  | [] -> (st, (s, i, o))
+  | inst :: p' -> match inst with
+    | BINOP op -> eval (eval_bin_op op (st, (s, i, o))) p'
+    | CONST n ->  eval (n :: st, (s, i, o)) p'
+    | READ -> eval (let z :: i' = i in (z :: st, (s, i', o))) p'
+    | WRITE -> eval (let z :: st' = st in (st, (s, i, o @ [z]))) p'
+    | LD x -> eval ((s x) :: st, (s, i, o)) p'
+    | ST x -> eval (let z :: st' = st in (st', (Language.Expr.update x z s, i, o))) p'
+
 
 (* Top-level evaluation
 
@@ -34,6 +47,13 @@ let eval _ = failwith "Not yet implemented"
 *)
 let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in o
 
+(* utility functions for 'compile' function *)
+
+let rec compileExpr expr = match expr with
+  | Language.Expr.Binop (op, x, y) -> compileExpr x @ compileExpr y @ [BINOP op]
+  | Language.Expr.Const n -> [CONST n]
+  | Language.Expr.Var x -> [LD x]
+
 (* Stack machine compiler
 
      val compile : Language.Stmt.t -> prg
@@ -41,4 +61,9 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+
+let rec compile t = match t with
+  | Language.Stmt.Assign (x, e) -> compileExpr e @ [ST x]
+  | Language.Stmt.Read x -> [READ; ST x]
+  | Language.Stmt.Write e -> compileExpr e @ [WRITE]
+  | Language.Stmt.Seq (s1, s2) -> compile s1 @ compile s2
