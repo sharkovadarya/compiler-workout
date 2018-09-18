07a329b8d564514531b0d93131beda4ab3160e55
sharkovadarya <sharkovadarya@gmail.com>
2018-09-18 11:11:18 +0300

    restoring files after an unfortunate removal of the previous repository

--- src/Expr.ml
+++ src/Expr.ml
@@ -36,12 +36,18 @@ let update x v s = fun y -> if x = y then v else s y
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"] *)
+
+(* Util functions for bool <-> int conversions *)
+
+let int_to_bool n = if n == 0 then false else true
+
+let bool_to_int b = if b then 1 else 0
 
 (* Expression evaluator
 
@@ -50,5 +56,23 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval state expr = match expr with
+  | Const n -> n
+  | Var x -> state x
+  | Binop (op, expr1, expr2) ->
+    let left = eval state expr1 in
+      let right = eval state expr2 in match op with
+        | "+" -> left + right
+        | "-" -> left - right
+        | "*" -> left * right
+        | "/" -> left / right
+        | "%" -> left mod right
+        | "<" -> bool_to_int (left < right)
+        | "<=" -> bool_to_int (left <= right)
+        | ">" -> bool_to_int (left > right)
+        | ">=" -> bool_to_int (left >= right)
+        | "==" -> bool_to_int (left == right)
+        | "!=" -> bool_to_int (left != right)
+        | "&&" -> bool_to_int ((int_to_bool left) && (int_to_bool right))
+        | "!!" -> bool_to_int ((int_to_bool left) || (int_to_bool right))
+        | _ -> failwith "undefined binary operator"
